a684062282dfc376a88896306510122f8a29a289
Marc Deely <themarcusdeely@gmail.com>
2019-07-12 22:58:29 -0500

    messing around with the camear

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ $( document ).ready(function() {
 	// Get access to the camera!
 	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 	    // Not adding `{ audio: true }` since we only want video now
-	    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+	    navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}}).then(function(stream) {
 	        //video.src = window.URL.createObjectURL(stream);
 	        video.srcObject = stream;
 	        video.play();
