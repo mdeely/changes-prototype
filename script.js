b2d5e57f01a82df856c435f8cb76cf5188b92cad
Marc Deely <themarcusdeely@gmail.com>
2019-07-12 23:58:26 -0500

    updated styling

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-	var $projectListSelector = $(".projectListSelector");
+	var $projectListSelector = $(".projectTitle__container:not(.settingsTitle)");
 	var $cameraOptions = $(".cameraOptions");
 	var $settings = $(".iconButton.settings");
 
@@ -40,35 +40,13 @@ $( document ).ready(function() {
 		$(".iconButton.settings").toggleClass("show");
 		$(".iconButton.share").toggleClass("show");
 		$(".appOverlay .projectTitle__container").toggleClass("purple");
+		$(".iconButton.camera").toggleClass("purple");
 	}
 
 	function displaySettings(event) {
 		$settings.toggleClass("active");
 		$(".settingsMenu").toggleClass('active');
 	}
-
-	// Grab elements, create settings, etc.
-	var video = document.getElementById('video');
-
-	// Get access to the camera!
-	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-	    // Not adding `{ audio: true }` since we only want video now
-	    navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}}).then(function(stream) {
-	        //video.src = window.URL.createObjectURL(stream);
-	        video.srcObject = stream;
-	        video.play();
-	    });
-	}
-
-	// Elements for taking the snapshot
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
-	var video = document.getElementById('video');
-
-	// Trigger photo take
-	document.getElementById("snap").addEventListener("click", function() {
-		context.drawImage(video, 0, 0, 640, 480);
-	});
 
 
 });
